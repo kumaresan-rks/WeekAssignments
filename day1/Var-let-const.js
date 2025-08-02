7ed89d78d37e5223707fd86505070903f9f36c6f
kumaresan-rks <rkumaresanrk19@gmail.com>
2025-08-02 08:13:38 +0530

    Add files via upload
    day1 ver-let-const

---
+++ day1/Var-let-const.js
@@ -0,0 +1,12 @@
+const browserName = "Chrome";
+
+function getBrowserName(){
+
+    if(browserName==="Chrome"){
+        var browserName = "edge" 
+        let browserName = "edge" 
+    }
+    console.log(browserName);   
+}
+
+getBrowserName();
